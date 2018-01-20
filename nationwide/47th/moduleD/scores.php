9d7f9effbbf16f6a22dbec539bd6422a1c07ddcb
L-Eli <l.lin860218@gmail.com>
2018-01-20 16:53:11 +0800

    update scores table style

--- nationwide/47th/moduleD/scores.php
+++ nationwide/47th/moduleD/scores.php
@@ -35,37 +35,39 @@
     <body>
         <div id="container-game" style="width:620px !important">
             <div class="row">
-                <div class="layout">
+                <div class="layout" style="height: 700px;">
                     <h2>排行榜</h2>
-                    <table class="level">
-                        <tr>
-                            <th>名次</th>
-                            <th></th>
-                            <th>暱稱</th>
-                            <th>移動次數</th>
-                        <tr>
-                        <?php
-                            $level = 0;
-                            $steps = 0;
-                            $same_steps = 0;
-                            foreach ($scores as $score) {
-                                $same_steps++;
-                                if ($score['steps'] > $steps) {
-                                    $level += $same_steps;
-                                    $same_steps = 0;
-                                    $steps = $score['steps'];
+                    <div class="level-scroll">
+                        <table class="level">
+                            <tr>
+                                <th>名次</th>
+                                <th></th>
+                                <th>暱稱</th>
+                                <th>移動次數</th>
+                            <tr>
+                            <?php
+                                $level = 0;
+                                $steps = 0;
+                                $same_steps = 0;
+                                foreach ($scores as $score) {
+                                    $same_steps++;
+                                    if ($score['steps'] > $steps) {
+                                        $level += $same_steps;
+                                        $same_steps = 0;
+                                        $steps = $score['steps'];
+                                    }
+                                    ?>
+                                    <tr>
+                                        <td><?= $level ?></td>
+                                        <td><img id="avatarDisplay" src="<?= $score['avatar_path'] ?>"></td>
+                                        <td><?= $score['nickname'] ?></td>
+                                        <td><?= $score['steps'] ?></td>
+                                    </tr>
+                                    <?php
                                 }
-                                ?>
-                                <tr>
-                                    <td><?= $level ?></td>
-                                    <td><img id="avatarDisplay" src="<?= $score['avatar_path'] ?>"></td>
-                                    <td><?= $score['nickname'] ?></td>
-                                    <td><?= $score['steps'] ?></td>
-                                </tr>
-                                <?php
-                            }
-                        ?>
-                    </table>
+                            ?>
+                        </table>
+                    </div>
                     <div class="desc left">
                         <a href="index.html" class="btn btn-secondary">回到設定頁面</a>
                     </div>
