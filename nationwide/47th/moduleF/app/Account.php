b5edba5c449c3cbc07e10f0ef279c38711d1b8ac
L-Eli <l.lin860218@gmail.com>
2018-01-22 19:58:18 +0800

    Revert "add randomTokenWithLength method into model"
    This reverts commit 975c2a03b626e2210680bf1032fbe917b2c824ce.

--- nationwide/47th/moduleF/app/Account.php
+++ nationwide/47th/moduleF/app/Account.php
@@ -47,4 +47,17 @@ class Account extends Model
             $account->attributes['token'] = $token;
         });
     }
+
+    /**
+     * The "randomTokenWithLength" method of the model.
+     *
+     * @return string
+     */
+    private function randomTokenWithLength($length)
+    {
+        $token = substr(md5(uniqid(rand())), 0, $length - 2);
+        $random_number = rand(0, 9);
+        $random_alphabet = chr(rand(97, 122));
+        return $token . $random_number . $random_alphabet;
+    }
 }
