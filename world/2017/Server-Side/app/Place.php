6e9d979e209ccf6bfd4f645d3cb59c8a1f8ed8e8
L-Eli <l.lin860218@gmail.com>
2018-02-22 16:05:53 +0800

    handling relation on delete

--- world/2017/Server-Side/app/Place.php
+++ world/2017/Server-Side/app/Place.php
@@ -25,4 +25,53 @@ class Place extends Model
     protected $table = 'places';
 
     public $timestamps = false;
+
+    /**
+     * Get the schedules which depart from this place.
+     */
+    public function schedulesDepartFromPlace()
+    {
+        return $this->hasMany(Schedule::class, 'from_place_id', 'place_id');
+    }
+
+    /**
+     * Get the schedules which arrive to this place.
+     */
+    public function schedulesArriveToPlace()
+    {
+        return $this->hasMany(Schedule::class, 'to_place_id', 'place_id');
+    }
+
+    /**
+     * Get the histories which depart from this place.
+     */
+    public function historiesDepartFromPlace()
+    {
+        return $this->hasMany(History::class, 'from_place_id', 'place_id');
+    }
+
+    /**
+     * Get the histories which arrive to this place.
+     */
+    public function historiesArriveToPlace()
+    {
+        return $this->hasMany(History::class, 'to_place_id', 'place_id');
+    }
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($place) {
+            $place->schedulesDepartFromPlace()->delete();
+            $place->schedulesArriveToPlace()->delete();
+            $place->historiesDepartFromPlace()->delete();
+            $place->historiesArriveToPlace()->delete();
+        });
+    }
 }
