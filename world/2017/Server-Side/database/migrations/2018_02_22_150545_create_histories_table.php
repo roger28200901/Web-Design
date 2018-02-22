370b042cc1235be57b9fc9567b2978c32fe19f51
L-Eli <l.lin860218@gmail.com>
2018-02-22 15:17:10 +0800

    initial histories table migration

---
+++ world/2017/Server-Side/database/migrations/2018_02_22_150545_create_histories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->default(0);
+            $table->string('from_place_id', 3)->default('');
+            $table->string('to_place_id', 3)->default('');
+            $table->text('schedule_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+}
