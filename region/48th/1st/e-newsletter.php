218054e9794f9531dbcce68f7bce0dcd9f63e718
林益弛 <L-Eli@hayashimasumasutayus-MacBook-Air.local>
2018-04-01 13:25:06 +0800

    add: e-newletter page

---
+++ region/48th/1st/e-newsletter.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>電子報製作系統</title>
+</head>
+
+<body>
+    <h1>電子報製作系統</h1>
+    <a href="index.php">返回</a>
+
+    <div id="content">
+        
+    </div>
+</body>
+
+</html>
+
