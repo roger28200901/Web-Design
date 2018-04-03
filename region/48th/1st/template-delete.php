73336387b828c4a9c513d3981d4f624888e89f6c
林益弛 <L-Eli@Elis-MacBook-Air.local>
2018-04-03 23:16:50 +0800

    update

--- region/48th/1st/template-delete.php
+++ region/48th/1st/template-delete.php
@@ -2,7 +2,10 @@
 
 include('config.php');
 
-$pdo->query(sprintf('delete from templates where id=%s', $_GET['id']));
+$template = $pdo->query(sprintf('select * from templates where id=%s', $_GET['id']))->fetch();
+unlink($template['path']);
+
+$pdo->query(sprintf('delete from templates where id=%s', $template['id']));
 
 header('location:template-index.php');
 
