f18a96d6fe9f4122c381ec20ec8b1c75db45c5ba
林益弛 <L-Eli@Elis-MacBook-Air.local>
2018-04-03 20:31:28 +0800

    update

--- region/48th/1st/template-store.php
+++ region/48th/1st/template-store.php
@@ -5,7 +5,7 @@ include('config.php');
 $path = sprintf("templates/%s.html", $_POST['name']);
 file_put_contents($path, $_POST['content']);
 
-$pdo->query(sprintf("insert into templates name, path, is_basic values '%s', '%s', 0",
+$pdo->query(sprintf("insert into templates (name, path, is_basic) values ('%s', '%s', 0)",
     $_POST['name'],
     $path
 ));
